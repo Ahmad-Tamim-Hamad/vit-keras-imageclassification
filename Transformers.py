f8314ccfdb2e84c7a64db1db38f1f73c766345d0
Ahmad Tamim Hamad <58792703+Tamim1992@users.noreply.github.com>
2023-08-25 10:05:34 -0500

    Update Transformers.py

--- Transformers.py
+++ Transformers.py
@@ -1,57 +1,56 @@
+
+def configure_gpu():
+    import tensorflow as tf
+    if tf.config.list_physical_devices('GPU'):
+        print("GPU is available.")
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    else:
+        print("GPU is not available.")
+
+configure_gpu()
+
+
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
-import tensorflow_addons as tfa
+import itertools
+from sklearn.metrics import confusion_matrix, classification_report
 
-class Config:
-    input_size = 32
-    input_shape = [input_size, input_size, 3]
-    learning_rate = 0.001
-    weight_decay = 0.0001
-    batch_size = 256
-    num_classes = 3
-    num_epochs = 100
-    image_size = 72
-    patch_size = 6
-    num_patches = (image_size // patch_size) ** 2
-    projection_dim = 64
-    num_heads = 4
-    transformer_units = [
-        projection_dim * 2,
-        projection_dim
-    ]
-    transformer_layers = 8
-    mlp_head_units = [2048, 1024]
 
-(train_data, train_labels), (test_data, test_labels) = keras.datasets.cifar10.load_data()
+num_classes = 3
+input_shape = (128, 128, 3)
+batch_size = 256
+train_data_dir = "/home/tamim/Desktop/Training_Testing_dataset-F62/Train_dataset"
+validation_data_dir = "/home/tamim/Desktop/Training_Testing_dataset-F62/Val_dataset"
+test_data_dir = "/home/tamim/Desktop/Training_Testing_dataset-F62/Test_dataset"
 
-# Select only three classes, for example classes 0, 1, and 2
-selected_classes = [0, 1, 2]
-train_indices = np.isin(train_labels, selected_classes).flatten()
-test_indices = np.isin(test_labels, selected_classes).flatten()
+#Parameters
+learning_rate = 0.001
+weight_decay = 0.0001
+image_size = 128
+patch_size = 8
+num_patches = (image_size // patch_size) ** 2
+projection_dim = 32
+num_heads = 4
+transformer_units = [projection_dim * 2, projection_dim]
+transformer_layers = 8
+mlp_head_units = [2048, 1024]
+dropout_rate = 0.5 
+num_epochs = 500
 
-train_data, train_labels = train_data[train_indices], train_labels[train_indices]
-test_data, test_labels = test_data[test_indices], test_labels[test_indices]
 
-print("Training data shape:", train_data.shape, train_labels.shape)
-print("Test data shape:", test_data.shape, test_labels.shape)
 
-augmentation_layer = tf.keras.Sequential([
-    keras.layers.Input(Config.input_shape),
-    keras.layers.experimental.preprocessing.Normalization(),
-    keras.layers.experimental.preprocessing.Resizing(Config.image_size, Config.image_size),
-    keras.layers.experimental.preprocessing.RandomRotation(factor=0.02),
-    keras.layers.experimental.preprocessing.RandomZoom(height_factor=0.2, width_factor=0.2),
-])
-
-augmentation_layer.layers[0].adapt(train_data)
+# Initialize the MirroredStrategy to use both gpu
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
 def mlp(x, hidden_units, dropout_rate):
     for units in hidden_units:
-        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dense(units, activation=tf.nn.gelu, kernel_regularizer=keras.regularizers.l2(weight_decay))(x)
         x = layers.Dropout(dropout_rate)(x)
     return x
 
@@ -70,137 +69,207 @@ class Patches(layers.Layer):
             padding="VALID",
         )
         patch_dims = patches.shape[-1]
-        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
-        return patches
+        return tf.reshape(patches, [batch_size, -1, patch_dims])
 
 class PatchEncoder(layers.Layer):
     def __init__(self, num_patches, projection_dim):
         super(PatchEncoder, self).__init__()
         self.num_patches = num_patches
-        self.projection = layers.Dense(projection_dim)
+        self.projection = layers.Dense(units=projection_dim)
         self.position_embedding = layers.Embedding(
             input_dim=num_patches, output_dim=projection_dim
         )
 
-    def call(self, patch):
+    def call(self, patches):
         positions = tf.range(start=0, limit=self.num_patches, delta=1)
-        encoded = self.projection(patch) + self.position_embedding(positions)
+        encoded = self.projection(patches) + self.position_embedding(positions)
         return encoded
 
-def create_vision_transformer():
-    inputs = layers.Input(shape=Config.input_shape)
-    augmented = augmentation_layer(inputs)
+def create_vit_classifier():
+    inputs = layers.Input(shape=input_shape)
+    augmented = data_augmentation(inputs)
+    patches = Patches(patch_size)(augmented)
+    encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
 
-    patches = Patches(Config.patch_size)(augmented)
-    encoder_patches = PatchEncoder(Config.num_patches, Config.projection_dim)(patches)
-
-    for _ in range(Config.transformer_layers):
-        x1 = layers.LayerNormalization(epsilon=1e-6)(encoder_patches)
+    for _ in range(transformer_layers):
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
         attention_output = layers.MultiHeadAttention(
-            num_heads=Config.num_heads,
-            key_dim=Config.projection_dim,
-            dropout=0.1
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
         )(x1, x1)
-        x2 = layers.Add()([attention_output, encoder_patches])
-
+        x2 = layers.Add()([attention_output, encoded_patches])
         x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
-        x3 = mlp(x3, hidden_units=Config.transformer_units, dropout_rate=0.1)
-        encoder_patches = layers.Add()([x3, x2])
+        x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+        encoded_patches = layers.Add()([x3, x2])
 
-    representation = layers.LayerNormalization(epsilon=1e-6)(encoder_patches)
+    representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
     representation = layers.Flatten()(representation)
     representation = layers.Dropout(0.5)(representation)
+    features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+    logits = layers.Dense(num_classes)(features)
+    return keras.Model(inputs=inputs, outputs=logits)
 
-    features = mlp(representation, hidden_units=Config.mlp_head_units, dropout_rate=0.5)
-    outputs = layers.Dense(Config.num_classes)(features)
+def run_experiment(model):
+    decayed_learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=learning_rate,
+        decay_steps=1000,
+        decay_rate=0.9,
+        staircase=True
+    )
+    
+    optimizer = keras.optimizers.Adam(learning_rate=decayed_learning_rate)
+    
+    model.compile(
+        optimizer=optimizer,
+        loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+        metrics=[
+            keras.metrics.CategoricalAccuracy(name="accuracy")
+        ],
+    )
 
-    model = keras.Model(inputs=inputs, outputs=outputs)
-    return model
+    checkpoint_filepath = "/tmp/checkpoint"
+    checkpoint_callback = keras.callbacks.ModelCheckpoint(
+        checkpoint_filepath,
+        monitor="val_accuracy",
+        save_best_only=True,
+        save_weights_only=True,
+    )
 
-keras.backend.clear_session()
-vit_classifier = create_vision_transformer()
-vit_classifier.summary()
+    history = model.fit(
+        train_generator,
+        epochs=num_epochs,
+        validation_data=validation_generator, 
+        callbacks=[checkpoint_callback],
+    )
 
-optimizer = tfa.optimizers.AdamW(
-    learning_rate=Config.learning_rate,
-    weight_decay=Config.weight_decay
-)
+    model.load_weights(checkpoint_filepath)
+    _, accuracy = model.evaluate(test_generator) 
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+    final_train_accuracy = history.history['accuracy'][-1]
+    final_val_accuracy = history.history['val_accuracy'][-1]
+    print(f"Final training accuracy: {round(final_train_accuracy * 100, 2)}%")
+    print(f"Final validation accuracy: {round(final_val_accuracy * 100, 2)}%")
 
-vit_classifier.compile(
-    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    optimizer=optimizer,
-    metrics=["accuracy"]
-)
-
-checkpoint_path = "model.h5"
-checkpoint = keras.callbacks.ModelCheckpoint(
-    checkpoint_path,
-    monitor="val_accuracy",
-    save_best_only=True,
-    save_weights_only=True
-)
-
-history = vit_classifier.fit(
-    train_data,
-    train_labels,
-    validation_data=(test_data, test_labels),
-    batch_size=Config.batch_size,
-    epochs=Config.num_epochs,
-    callbacks=[checkpoint],
-)
-
-# Plot the training and validation accuracy
-plt.plot(history.history['accuracy'], label='Training Accuracy')
-plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-plt.title('Training and Validation Accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
-
-# Add space between the two plots
-plt.figure()
-
-plt.figure()
-
-# Plot the training and validation loss
-plt.plot(history.history['loss'], label='Training Loss')
-plt.plot(history.history['val_loss'], label='Validation Loss')
-plt.title('Training and Validation Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-
-import matplotlib.pyplot as plt
-
-# Set a style for the plots
-plt.style.use('seaborn-darkgrid')
-
-# Get the test accuracy and test loss
-test_accuracy = history.history['val_accuracy'][-1]
-test_loss = history.history['val_loss'][-1]
-
-# Create a bar plot with two bars
-fig, ax = plt.subplots()
-ax.bar(['Test Accuracy'], [test_accuracy], color='dodgerblue', label='Test Accuracy')
-ax.bar(['Test Loss'], [test_loss], color='darkorange', label='Test Loss')
-
-# Add text labels to the bars
-ax.text(0, test_accuracy, f"{test_accuracy:.2f}", ha='center', va='bottom', fontweight='bold')
-ax.text(1, test_loss, f"{test_loss:.4f}", ha='center', va='bottom', fontweight='bold')
-
-# Set the y-axis limits
-ax.set_ylim(0, max(test_accuracy, test_loss) * 1.1)
-
-# Add labels and title
-plt.xlabel('Metrics')
-plt.ylabel('Value')
-plt.title('Test Accuracy and Test Loss')
-plt.legend()
-
-# Show the plot
-plt.show()
+    return history
 
 
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+  
+    plt.figure(figsize=(4,4))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+def evaluate_model(model, test_generator):
+    # getting the true labels and predictions
+    y_true = test_generator.classes
+    y_pred = model.predict(test_generator, verbose=1)
+    y_pred_classes = np.argmax(y_pred, axis=1)
+
+    cm = confusion_matrix(y_true, y_pred_classes)
+    plot_confusion_matrix(cm, classes=test_generator.class_indices.keys())
+    
+    # to print classification report we are using this code
+    print(classification_report(y_true, y_pred_classes, target_names=test_generator.class_indices.keys()))
+
+
+    
+    
+if __name__ == "__main__":
+    
+    configure_gpu()
+
+    # Data generators
+    train_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+    validation_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+    test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=input_shape[:2],
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=True
+    )
+
+    validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=input_shape[:2],
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False
+    )
+
+    test_generator = test_datagen.flow_from_directory(
+        test_data_dir,
+        target_size=input_shape[:2],
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False
+    )
+    
+    # Visualization code 
+    x_train_batch, _ = next(train_generator)
+    
+    plt.figure(figsize=(4, 4))
+    image = x_train_batch[np.random.choice(range(x_train_batch.shape[0]))]
+    plt.imshow(image)
+    plt.axis("off")
+    plt.show() 
+
+    resized_image = tf.image.resize(
+        tf.convert_to_tensor([image]), size=(image_size, image_size)
+    )
+    patches = Patches(patch_size)(resized_image)
+    
+    n = int(np.sqrt(patches.shape[1]))
+    plt.figure(figsize=(4, 4))
+    for i, patch in enumerate(patches[0]):
+        ax = plt.subplot(n, n, i + 1)
+        patch_img = tf.reshape(patch, (patch_size, patch_size, 3))
+        plt.imshow(patch_img.numpy())
+        plt.axis("off")
+    plt.show()
+
+    print(f"Train samples: {train_generator.samples}, Test samples: {test_generator.samples}")
+
+    with strategy.scope():
+        # Model-related code
+        data_augmentation = keras.Sequential(
+            [
+                layers.Lambda(lambda x: (x - [0.5, 0.5, 0.5]) / [0.5, 0.5, 0.5]),
+                layers.experimental.preprocessing.Resizing(image_size, image_size),
+                layers.experimental.preprocessing.RandomFlip("horizontal"),
+                layers.experimental.preprocessing.RandomRotation(factor=0.02),
+                layers.experimental.preprocessing.RandomZoom(height_factor=0.2, width_factor=0.2)
+            ],
+            name="data_augmentation"
+        )
+
+        vit_classifier = create_vit_classifier()
+        history = run_experiment(vit_classifier)
+
+    evaluate_model(vit_classifier, test_generator) 
 
